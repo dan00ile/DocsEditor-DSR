5c909f9c4daedcc6290ac1bfceafc392e5cc7dac
DanJoni <DanJoni@yandex.ru>
2025-06-30 23:39:04 +0300

    init

---
+++ src/main/java/dsr/practice/docseditor/DocsEditorApplication.java
@@ -0,0 +1,13 @@
+package dsr.practice.docseditor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocsEditorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DocsEditorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dsr/practice/docseditor/DocsEditorApplicationTests.java
@@ -0,0 +1,13 @@
+package dsr.practice.docseditor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocsEditorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
