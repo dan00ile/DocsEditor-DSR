35b544dad0cd6599b070437c9aebcc3fac5964fb
DanJoni <DanJoni@yandex.ru>
2025-07-06 17:44:35 +0300

    feat(dto): add dto's for auth

---
+++ src/main/java/dsr/practice/docseditor/dto/LoginRequest.java
@@ -0,0 +1,22 @@
+package dsr.practice.docseditor.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginRequest {
+    @Size(min = 5, max = 50, message = "Имя пользователя должно содержать от 5 до 50 символов")
+    @NotBlank(message = "Имя пользователя не может быть пустыми")
+    private String username;
+
+    @Size(min = 8, max = 255, message = "Длина пароля должна быть от 8 до 255 символов")
+    @NotBlank(message = "Пароль не может быть пустыми")
+    private String password;
+}
---
+++ src/main/java/dsr/practice/docseditor/dto/RefreshTokenRequest.java
@@ -0,0 +1,16 @@
+package dsr.practice.docseditor.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class RefreshTokenRequest {
+    @NotBlank(message = "Refresh токен не может быть пустым")
+    private String refreshToken;
+}
